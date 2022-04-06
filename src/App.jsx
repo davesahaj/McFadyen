7968fd734d793385fdfc57313f23fd1e73e794a8
davesahaj <sahajdavework@pm.me>
2022-04-06 10:45:55 +0530

    added Product page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Layout/Header";
+import ProductDetails from "./components/Product/ProductPage";
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <ProductDetails />
+    </div>
+  );
+}
+
+export default App;
